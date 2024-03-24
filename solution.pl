c1bc37c7c8d16355c5cbdc44135e92dfbf988b68
Abdallah Hussein <abiza2013@gmail.com>
2024-03-24 22:41:50 +0200

    Implemented Task-7

--- solution.pl
+++ solution.pl
@@ -13,3 +13,17 @@ isBoycott(ItemOrCompany) :-
 % If the item does not have an alternative, check if the company is listed in the boycott_company facts
 isBoycott(ItemOrCompany) :-
     boycott_company(ItemOrCompany, _).
+
+% _____________________ Task 7 ___________________________________________________
+% Given the company name or an item name, find the justification why you need to boycott this company/item.
+
+whyToBoycott(ItemOrCompany, Justification) :-
+    % Check if the given item or company directly appears in the boycott_company facts.
+    % if found, return corresponding justification.
+    boycott_company(ItemOrCompany, Justification),
+    !.
+whyToBoycott(ItemOrCompany, Justification) :-
+    % get the company associated with the given item.
+    item(ItemOrCompany, Company, _),
+    % Check if the company is in the boycott_company facts.
+    boycott_company(Company, Justification).
