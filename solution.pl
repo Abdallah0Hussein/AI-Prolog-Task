a4be77d1585f5c524aaa4d04df2674153b6e1884
Abdallah Hussein <abiza2013@gmail.com>
2024-03-25 07:22:45 +0200

    Edit Task-6

--- solution.pl
+++ solution.pl
@@ -28,12 +28,11 @@ calcPriceOfItems([Item|RestItems], Accumulator, TotalPrice):-
 % Given the item name or company name, determine whether we need to boycott or not.
 
 isBoycott(ItemOrCompany) :-
-    % Check if the item has an alternative
-    % If found, then this item is considered boycotted, so return true.
-    alternative(ItemOrCompany, _),
-    % Cut operator to prevent conutinuing if an alternative is found
+    item(ItemOrCompany, Company, _),
+    isBoycott(Company),
+    % Cut operator to prevent conutinuing
     !.
-% If the item does not have an alternative, check if the company is listed in the boycott_company facts
+
 isBoycott(ItemOrCompany) :-
     boycott_company(ItemOrCompany, _).
 
